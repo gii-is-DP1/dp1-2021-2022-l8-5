b004d27e7bb261f6956c26ba85e95653886e3da1
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2021-11-15 16:09:38 +0100

    Board view with cards

--- src/main/java/org/springframework/dwarf/mountain_card/MountainDeckService.java
+++ src/main/java/org/springframework/dwarf/mountain_card/MountainDeckService.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Diego Ruiz Gil
  */
+@Service
 public class MountainDeckService {
 	
 	private MountainDeckRepository mountainDeckRepo;
