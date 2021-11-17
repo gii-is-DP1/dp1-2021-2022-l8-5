980f7b70ac3649d2a104ba62206bc8eb1e8ca8ef
xiscomigueles <miguelesdominguez@gmail.com>
2021-11-17 01:32:09 +0100

    Controller,Service and Repository about Special_Deck

---
+++ src/main/java/org/springframework/dwarf/special_card/SpecialDeckRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.dwarf.special_card;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialDeckRepository extends CrudRepository<SpecialDeck, Integer> {
+
+}
