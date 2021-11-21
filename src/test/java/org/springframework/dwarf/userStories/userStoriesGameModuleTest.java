8909952105e90d22472d5a5f8622f297776a612a
Vell <80247733+Dr-Vell@users.noreply.github.com>
2021-11-21 19:46:38 +0100

    Implemented Login tests
    Co-Authored-By: José Ignacio <80249164+josgarboh@users.noreply.github.com>
    Co-Authored-By: Pablo Álvarez <80222536+Pabalvcar@users.noreply.github.com>
    Co-Authored-By: TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>

--- src/test/java/org/springframework/dwarf/userStories/userStoriesGameModuleTest.java
+++ src/test/java/org/springframework/dwarf/userStories/userStoriesGameModuleTest.java
@@ -1,4 +1,4 @@
-package org.springframework.dwarf.userStoriesGameModule;
+package org.springframework.dwarf.userStories;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
