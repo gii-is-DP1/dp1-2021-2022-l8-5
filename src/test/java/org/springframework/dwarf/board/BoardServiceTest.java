783847bcc0c1f0900f861a3e96299db8703e7cc8
xiscomigueles <miguelesdominguez@gmail.com>
2021-11-23 12:54:45 +0100

    SpecialDeckServiceTest

--- src/test/java/org/springframework/dwarf/board/BoardServiceTest.java
+++ src/test/java/org/springframework/dwarf/board/BoardServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,18 +25,21 @@ public class BoardServiceTest {
 	private MountainDeckService mountaindesckservice;
 	
 	@Test
+	@DisplayName("")
 	void testBoardCount() {
 		int count = boardservice.boardCount();
 		assertThat(count).isEqualTo(2);
 	}
 	
 	@Test
+	@DisplayName("Returns all boards")
 	void testFindAllBoard() {
 		Iterable<Board> iterator = boardservice.findAll();
 		assertThat(iterator.spliterator().getExactSizeIfKnown()).isEqualTo(2); 
 	}
 	
 	@Test 
+	@DisplayName("Returns a board by its Id correctly")
 	void testFinByBoardId() {
 		int boardid=1;
 		Optional<Board> board= boardservice.findByBoardId(boardid);
@@ -43,6 +47,7 @@ public class BoardServiceTest {
 		assertThat(b.getBackground()).isEqualTo("resources/images/oro_erebor.jpg");
 	}
 	@Test
+	@DisplayName("Delete a board")
 	void testDeleteBoard() {
 		int boardid=2;
 		Optional<Board> board= boardservice.findByBoardId(boardid);
@@ -56,6 +61,7 @@ public class BoardServiceTest {
 	}
 	
 	@Test
+	@DisplayName("Save a board")
 	void testSaveBoard() {
 		Board board= new Board();
 		board.setMountainDeck(mountaindesckservice.findByMountainDeckId(1).get());
---
+++ src/test/java/org/springframework/dwarf/special_card/SpecialDeckServiceTest.java
@@ -0,0 +1,84 @@
+package org.springframework.dwarf.special_card;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dwarf.board.Board;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class SpecialDeckServiceTest {
+	
+	@Autowired
+	private SpecialDeckService specialDeckService;
+	@Autowired
+	private SpecialCardService specialCardService;
+	
+	@Test
+	@DisplayName("")
+	void testSpecialDeckServiceCount() {
+		int count = specialDeckService.specialDeckCount();
+		assertThat(count).isEqualTo(2);
+	}
+	@Test
+	@DisplayName("Returns all special_decks")
+	void testFindAllSpecialDecks() {
+		Iterable<SpecialDeck> iterator = specialDeckService.findAll();
+		assertThat(iterator.spliterator().getExactSizeIfKnown()).isEqualTo(2); 
+	}
+	
+	@Test 
+	@DisplayName("Returns a special_deck by its Id correctly")
+	void testFinBySpecialDeckId() {
+		int specialdeckid=1;
+		Optional<SpecialDeck> sp= specialDeckService.findBySpecialDeckId(specialdeckid);
+		if(sp.isPresent()) {
+			assertThat(sp.get().getXPosition()).isEqualTo(0);
+			assertThat(sp.get().getYPosition()).isEqualTo(0);
+		}else {
+			System.out.println("Special deck not found");
+		}
+	
+	}
+	@Test
+	@DisplayName("Delete a special deck")
+	void testDeleteSpecialDeck() {
+		int spid=2;
+		Optional<SpecialDeck> sp= specialDeckService.findBySpecialDeckId(spid);
+		if(sp.isPresent()) {
+			specialDeckService.delete(sp.get());
+			Optional<SpecialDeck> spdeleted =specialDeckService.findBySpecialDeckId(spid);
+			assertThat(spdeleted.isPresent()).isFalse();
+		}else {
+			System.out.println("Special deck not found");
+		}	
+	}
+	@Test
+	@DisplayName("Save a special deck")
+	void testSaveSpecialDeck() {
+		SpecialDeck sp = new SpecialDeck();
+		sp.setXPosition(0);
+		sp.setYPosition(2);
+		List<SpecialCard> cards = new ArrayList<SpecialCard>();
+		specialCardService.findAll().forEach(cards::add);
+		sp.setSpecialCard(cards);
+		
+		specialDeckService.saveSpecialDeck(sp);
+		int spid=sp.getId();
+		Optional<SpecialDeck> sptest =specialDeckService.findBySpecialDeckId(spid);
+		assertThat(sptest.isPresent()).isTrue();
+			
+	}	
+
+}
