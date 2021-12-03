51ffe890241b0966dead11f57dd83156128cdd34
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2021-12-03 16:17:35 +0100

    Unified updatePlayer

--- src/main/java/org/springframework/dwarf/player/PlayerController.java
+++ src/main/java/org/springframework/dwarf/player/PlayerController.java
@@ -144,25 +144,7 @@ public class PlayerController {
 			return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
 		}
 		else {
-			Player playerFound = playerService.findPlayerById(playerid);
-			User userFound = playerFound.getUser();
-			BeanUtils.copyProperties(player,playerFound,"id");
-			User user2 = player.getUser();
-			BeanUtils.copyProperties(user2,userFound,"username","authorities");
-			playerFound.setUser(userFound);
-				try {
-					//creating owner, user and authorities
-					this.playerService.savePlayer(playerFound);
-					
-					return "redirect:/players";
-				} catch (DuplicatedUsernameException dp) {
-					result.rejectValue (" name", " duplicate", "already exists");
-					return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
-					} catch (DuplicatedEmailException dp) {
-						result.rejectValue (" email", " duplicate", "already exists");
-						return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
-						}
-				
+			return updatingPlayer(playerid, player, result);
 		}
 	}
 
@@ -186,26 +168,28 @@ public class PlayerController {
 			return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
 		}
 		else {
-			Player playerFound = playerService.findPlayerById(playerid);
-			User userFound = playerFound.getUser();
-			BeanUtils.copyProperties(player,playerFound,"id");
-			User user2 = player.getUser();
-			BeanUtils.copyProperties(user2,userFound,"username","authorities");
-			playerFound.setUser(userFound);
-			try {
-				//creating owner, user and authorities
-				this.playerService.savePlayer(playerFound);
-				
-				return "redirect:/";
-			} catch (DuplicatedUsernameException dp) {
-				result.rejectValue (" name", " duplicate", "already exists");
+			return updatingPlayer(playerid, player, result);
+	}
+	}
+	private String updatingPlayer(Integer playerid,Player player,BindingResult result) {
+		Player playerFound = playerService.findPlayerById(playerid);
+		User userFound = playerFound.getUser();
+		BeanUtils.copyProperties(player,playerFound,"id");
+		User user2 = player.getUser();
+		BeanUtils.copyProperties(user2,userFound,"username","authorities");
+		playerFound.setUser(userFound);
+		try {
+			this.playerService.savePlayer(playerFound);
+			
+			return "redirect:/";
+		} catch (DuplicatedUsernameException dp) {
+			result.rejectValue (" name", " duplicate", "already exists");
+			return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
+			} catch (DuplicatedEmailException dp) {
+				result.rejectValue (" email", " duplicate", "already exists");
 				return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
-				} catch (DuplicatedEmailException dp) {
-					result.rejectValue (" email", " duplicate", "already exists");
-					return VIEWS_PLAYER_CREATE_OR_UPDATE_FORM;
-					}
-			//this.playerService.savePlayer(player);
-		}
+				}
+		//this.playerService.savePlayer(player);
 	}
 	
 	/**
