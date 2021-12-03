70ab24547fb15d7325f46042a166aba8bb5ee2a3
Pablo Álvarez <80222536+Pabalvcar@users.noreply.github.com>
2021-12-03 17:16:51 +0100

    Added/fixed resources query
    Cool commit
    
    Co-Authored-By: José Ignacio <80249164+josgarboh@users.noreply.github.com>

--- src/main/java/org/springframework/dwarf/resources/ResourcesRepository.java
+++ src/main/java/org/springframework/dwarf/resources/ResourcesRepository.java
@@ -1,8 +1,15 @@
 package org.springframework.dwarf.resources;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ResourcesRepository extends  CrudRepository<Resources, Integer>{
+	
+    @Query("SELECT resources FROM Resources resources WHERE resources.game.id =:gameId")
+    public Collection<Resources> findByGameId(@Param("gameId") int gameId);
     
 }
