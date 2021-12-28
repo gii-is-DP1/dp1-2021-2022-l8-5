2a2fb85da4c4a3fe353083a1a8e88d645ff80507
Pablo Álvarez <80222536+Pabalvcar@users.noreply.github.com>
2021-12-28 12:59:32 +0100

    Updated state pattern

---
+++ src/main/java/org/springframework/dwarf/game/ActionResolution.java
@@ -0,0 +1,50 @@
+package org.springframework.dwarf.game;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jpatterns.gof.StatePattern;
+import org.jpatterns.gof.CompositePattern.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dwarf.game.GameState.GamePhase;
+import org.springframework.dwarf.player.Player;
+import org.springframework.dwarf.worker.Worker;
+import org.springframework.dwarf.worker.WorkerService;
+
+@StatePattern.ConcreteState
+@Component
+public class ActionResolution implements GamePhase{
+    
+    @Autowired
+    private GameService gameService;
+    
+    @Autowired
+    private WorkerService workerService;
+
+	@Override
+	public void phaseResolution(Game game) {
+		
+		Integer gameId = game.getId();
+		List<Player> players = gameService.searchPlayersByGame(gameId);	//Todos los jugadores del game
+		
+		for(int i=0; i<players.size(); i++) {
+			List<Worker> workers = workerService.findByPlayerIdAndGameId(gameId, players.get(i).getId())
+												.stream().collect(Collectors.toList());
+			for(int j=0; j<workers.size(); j++) {
+				Worker w = workers.get(j);		//Worker j del player i;
+				Integer wXPos = w.getXposition();
+				Integer wYPos = w.getYposition();
+				
+				//Comprobaciones de posiciones y resolucion de acciones de las cartas pendiente (Strategy)
+				
+			}
+		}
+		
+		game.setPhase(new MineralExtraction());
+	}
+
+	@Override
+	public GamePhaseEnum getPhaseName() {
+		return GamePhaseEnum.ACTION_RESOLUTION;
+	}
+}
---
+++ src/main/java/org/springframework/dwarf/game/MineralExtraction.java
@@ -0,0 +1,78 @@
+package org.springframework.dwarf.game;
+
+import java.util.List;
+import java.util.Random;
+
+import org.jpatterns.gof.StatePattern;
+import org.jpatterns.gof.CompositePattern.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dwarf.board.Board;
+import org.springframework.dwarf.board.BoardCell;
+import org.springframework.dwarf.board.BoardCellService;
+import org.springframework.dwarf.game.GameState.GamePhase;
+import org.springframework.dwarf.mountain_card.MountainCard;
+import org.springframework.dwarf.mountain_card.MountainDeck;
+import org.springframework.dwarf.mountain_card.MountainDeckService;
+import org.springframework.dwarf.web.LoggedUserController;
+
+@StatePattern.ConcreteState
+@Component
+public class MineralExtraction implements GamePhase{
+	
+    @Autowired
+    private GameService gameService;
+    
+    @Autowired
+    private MountainDeckService mountainDeckService;
+    
+    @Autowired
+    private BoardCellService boardCellService;
+
+    @Override
+    public void phaseResolution(Game game) {
+    	
+	/*	// runs only once
+		if(game.getFirstPlayer()!=LoggedUserController.loggedPlayer())
+			return;
+    	// picks two randoms cards
+        MountainDeck mountaindeck = gameService.searchDeckByGameId(game.getId()).get();
+        List<MountainCard> mountaincards = mountaindeck.getMountainCards();
+        Random random = new Random();
+        
+        int index1 = random.nextInt(mountaincards.size());
+        MountainCard mountaincard1 = mountaincards.remove(index1);
+        
+        int index2 = random.nextInt(mountaincards.size());
+        MountainCard mountaincard2 = mountaincards.remove(index2);
+        
+        mountainDeckService.saveMountainDeck(mountaindeck);
+        
+        // set cards
+    	Board board = gameService.findBoardByGameId(game.getId()).get();
+    	
+    	List<BoardCell> boardcells = board.getBoardCells();
+    	
+    	for (BoardCell b : boardcells) {
+    		setCard(mountaincard1, b);
+    		setCard(mountaincard2, b);
+    	}
+    	*/
+    	
+    	game.setPhase(new ActionSelection());
+    }
+    
+    private void setCard(MountainCard mountaincard, BoardCell boardcell) {
+		List<MountainCard> cellcards = boardcell.getMountaincards();
+		if (mountaincard.getXPosition().equals(boardcell.getXposition()) &&
+				mountaincard.getYPosition().equals(boardcell.getYposition())) {
+			cellcards.add(0, mountaincard);
+			boardcell.setMountaincards(cellcards);
+			boardCellService.saveBoardCell(boardcell);
+		}
+    }
+
+	@Override
+	public GamePhaseEnum getPhaseName() {
+		return GamePhaseEnum.MINERAL_EXTRACTION;
+	}
+}
