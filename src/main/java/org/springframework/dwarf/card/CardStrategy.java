9007837deef62df0d489dd664c8625bc46e4c7c9
José Ignacio <garciaji700@gmail.com>
2021-12-30 18:28:53 +0100

    package specialCardStrategy completed

---
+++ src/main/java/org/springframework/dwarf/card/CardStrategy.java
@@ -0,0 +1,14 @@
+package org.springframework.dwarf.card;
+
+import org.jpatterns.gof.StrategyPattern;
+import org.springframework.dwarf.player.Player;
+import org.springframework.dwarf.strategies.CardStrategy.StrategyName;
+
+@StrategyPattern.Strategy
+public interface CardStrategy {
+
+	void actions(Player player);
+
+	StrategyName getName();
+	
+}
---
+++ src/main/java/org/springframework/dwarf/specialCardStrategy/HoldACouncil.java
@@ -0,0 +1,68 @@
+package org.springframework.dwarf.specialCardStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jpatterns.gof.StrategyPattern;
+import org.springframework.dwarf.board.Board;
+import org.springframework.dwarf.board.BoardCell;
+import org.springframework.dwarf.board.BoardCellService;
+import org.springframework.dwarf.board.BoardService;
+import org.springframework.dwarf.card.CardStrategy;
+import org.springframework.dwarf.game.Game;
+import org.springframework.dwarf.game.GameService;
+import org.springframework.dwarf.mountain_card.MountainCard;
+import org.springframework.dwarf.mountain_card.MountainDeckService;
+import org.springframework.dwarf.player.Player;
+import org.springframework.dwarf.strategies.CardStrategy.StrategyName;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StrategyPattern.ConcreteStrategy
+public class HoldACouncil implements CardStrategy {
+	
+	GameService gameService;
+	BoardService boardService;
+	BoardCellService boardCellService;
+	MountainDeckService mountainDeckService;
+	
+	@Override
+	public void actions(Player player) {
+		log.debug(player.getUsername() + ", con id" + player.getId() + ", ha realizado la accion " + this.getName().toString());
+		/*
+		Game currentGame = gameService.findPlayerUnfinishedGames(player).get();
+		Board board = gameService.findBoardByGameId(currentGame.getId()).get();
+		// se podria hacer una query que te diese el mazo de montaña con el game id
+		MountainDeck mountainDeck = board.getMountainDeck();
+		List<MountainCard> removedCards = this.removeTopCards(currentGame);
+		
+		mountainDeck.getMountainCards().addAll(removedCards);
+		mountainDeckService.saveMountainDeck(mountainDeck);
+		*/
+	}
+	
+	private List<MountainCard> removeTopCards(Game currentGame){
+		List<MountainCard> cardsToRemove = new ArrayList<MountainCard>();
+		Board board = gameService.findBoardByGameId(currentGame.getId()).get();
+		
+		for(BoardCell boardCell: board.getBoardCells()) {
+			List<MountainCard> listRemoveTop = boardCell.getMountaincards();
+			MountainCard cardRemoved = listRemoveTop.remove(0);
+			
+			boardCell.setMountaincards(listRemoveTop);
+			cardsToRemove.add(cardRemoved);
+			
+			boardCellService.saveBoardCell(boardCell);
+		}
+		
+		
+		return cardsToRemove;
+	}
+
+	@Override
+	public StrategyName getName() {
+		return StrategyName.HOLD_COUNCIL;
+	}
+	
+}
